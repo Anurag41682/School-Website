b5f9e3868c842c35519e2d1061ebe64634684f9f
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-23 18:07:22 +0530

    Set Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./section/Home";
+import About from "./section/About";
+import Academics from "./section/Academics";
+import Admissions from "./section/Admissions";
+import Contact from "./section/Contact";
+import Faculty from "./section/Faculty";
+import Gallery from "./section/Gallery";
+import Students from "./section/Students";
+import Navbar from "./section/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/academics" element={<Academics />} />
+          <Route path="/admissions" element={<Admissions />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/faculty" element={<Faculty />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/students" element={<Students />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/section/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="flex gap-4">
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}> About</Link>
+      <Link to={"/academics"}> Academics</Link>
+      <Link to={"/admissions"}> Admissions</Link>
+      <Link to={"/contact"}> Contact</Link>
+      <Link to={"/faculty"}> Faculty</Link>
+      <Link to={"/gallery"}> Gallery</Link>
+      <Link to={"/students"}>Students</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
