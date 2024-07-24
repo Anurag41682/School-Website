7d5a6e2ea084838b81683d9348cc28cc5b1927aa
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-24 10:07:05 +0530

    centered nav content

--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex gap-4 p-4 bg-slate-500 justify-around ">
+    <nav className="flex gap-4 p-4 bg-slate-500 justify-center">
       <Link to={"/"}>Home</Link>
-      <Link to={"/about"}> About</Link>
-      <Link to={"/academics"}> Academics</Link>
-      <Link to={"/admissions"}> Admissions</Link>
-      <Link to={"/contact"}> Contact</Link>
-      <Link to={"/faculty"}> Faculty</Link>
-      <Link to={"/gallery"}> Gallery</Link>
-      <Link to={"/students"}>Students</Link>
+      <Link to={"/about"}>About</Link>
+      <Link to={"/academics"}>Academics</Link>
+      <Link to={"/admissions"}>Admissions</Link>
+      <Link to={"/contact"}>Contact</Link>
+      <Link to={"/faculty"}>Faculty</Link>
+      <Link to={"/gallery"}>Gallery</Link>
+      <Link>Students</Link>
     </nav>
   );
 };
