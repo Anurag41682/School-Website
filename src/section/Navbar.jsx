aaf80bb03910fff1b18141456d94bec61ba24837
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-24 14:57:04 +0530

    Fixed navbar

--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex gap-4 p-4 bg-slate-500 justify-center">
+    <nav className="flex gap-4 p-4 bg-slate-500 justify-center fixed top-0 right-0 left-0">
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
       <Link to={"/academics"}>Academics</Link>
