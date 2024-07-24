223c2ce9aa735a588e43f8e13e29b17eeca642e9
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-24 12:32:41 +0530

    Changed Font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -71,7 +71,11 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ['"JetBrains Mono"', "monospace"],
+        mono: ['"JetBrains Mono"', "monospace"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
