62a43520807ea443935a2e5f70503c89b8f200b8
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-25 11:58:30 +0530

    Changed font of heading and Para for responsivness

--- src/components/CurriculumCard.jsx
+++ src/components/CurriculumCard.jsx
@@ -6,12 +6,14 @@ const CurriculumCard = ({ item }) => {
     <>
       <Card className="mt-10 p-5">
         <CardContent>
-          <h2 className="text-xl mb-3 ">{item.h2}</h2>
-          <p className="mb-2">
+          <h2 className="text-xl max-md:text-lg max-sm:text-base mb-3 ">
+            {item.h2}
+          </h2>
+          <p className="mb-2 max-md:text-sm">
             <strong>{item.s1}</strong>
             {item.p1}
           </p>
-          <p>
+          <p className="max-md:text-sm">
             <strong>{item.s2}</strong>
             {item.p2}
           </p>
