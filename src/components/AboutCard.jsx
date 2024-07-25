57d76b1176e6cac6cfe2eca3024f68789f92169e
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-25 09:59:17 +0530

    Updated Styles for small screens for responsivness

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -3,22 +3,22 @@ const AboutCard = ({ title, content, image, index }) => {
     <div>
       <h1
         className="text-3xl text-center mt-10 underline underline-offset-8
-"
+max-md:text-2xl max-sm:text-xl"
       >
         {title}
       </h1>
       <div
-        className={`flex img-content mt-5 gap-10  ${
+        className={`flex img-content items-center mt-5 gap-10 max-md:gap-5 max-sm:flex-col-reverse max-sm:mb-20  ${
           index % 2 ? "flex-row-reverse" : "flex-row"
         } `}
       >
         <p
-          style={{ wordSpacing: "8px" }}
-          className="leading-7  tracking-wider w-3/4"
+          style={{ wordSpacing: "5px" }}
+          className="leading-7  tracking-wider max-md:leading-5 max-md:text-sm max-md:tracking-wide"
         >
           {content}
         </p>
-        <img className="w-1/4 rounded-md" src={image}></img>
+        <img className="w-36 h-36 rounded-md" src={image}></img>
       </div>
     </div>
   );
