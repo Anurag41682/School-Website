420a32d57044d8169a1a50d067c6081920a7b61e
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-25 12:00:17 +0530

    Updated Image to fit box size and flex change on different size

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -1,9 +1,9 @@
 import { Card, CardContent } from "./ui/card";
 const SubjectCard = ({ name, image }) => {
   return (
-    <Card className="flex justify-center w-1/3">
-      <CardContent className="p-3">
-        <img src={image} alt={name}></img>
+    <Card className="flex justify-center w-1/3 max-md:w-3/4 ">
+      <CardContent className="p-3 w-full h-full">
+        <img src={image} alt={name} className="w-full h-full object-fill"></img>
       </CardContent>
     </Card>
   );
