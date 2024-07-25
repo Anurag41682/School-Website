b0bc07b0f214a73c3e1faafec6d5a2ec1951b6a3
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-25 11:59:16 +0530

    Added padding for small screen sizes

--- src/section/Academics/Academics.jsx
+++ src/section/Academics/Academics.jsx
@@ -4,14 +4,14 @@ import SubjectCard from "@/components/SubjectCard";
 import * as data from "./data";
 const Academics = () => {
   return (
-    <section className="px-20 my-20 text-slate-200">
-      <h1 className="text-3xl text-center underline underline-offset-8">
+    <section className="px-20 my-20 text-slate-100 max-md:px-5">
+      <h1 className="text-3xl max-md:text-2xl max-sm:text-xl text-center underline underline-offset-8">
         Curriculum
       </h1>
       {data.dataCurriculum.map((item, index) => {
         return <CurriculumCard key={index} item={item} />;
       })}
-      <h1 className="text-3xl text-center underline underline-offset-8 mt-20">
+      <h1 className="text-3xl max-md:text-2xl max-sm:text-xl   text-center underline underline-offset-8 mt-20">
         Subjects Taught
       </h1>
       <div className="flex flex-wrap gap-14 justify-around my-10 ">
