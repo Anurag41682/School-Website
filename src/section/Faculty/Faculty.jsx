ef275d347a778b0bce6cf67274864165f97e6c88
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-26 13:59:01 +0530

    updated padding for small screen

--- src/section/Faculty/Faculty.jsx
+++ src/section/Faculty/Faculty.jsx
@@ -3,7 +3,7 @@ import data from "./data";
 import FacultyCard from "@/components/FacultyCard";
 const Faculty = () => {
   return (
-    <section className="flex flex-col gap-7 px-20 my-20 text-white justify-center items-center">
+    <section className="flex flex-col gap-7 px-20 my-20 text-white justify-center items-center max-md:px-5">
       {data.map((item, index) => (
         <FacultyCard key={index} item={item} />
       ))}
