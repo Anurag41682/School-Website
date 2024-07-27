13d4a085692851c11f0e5560af73d00d3d4f899c
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-27 11:30:13 +0530

    centerd elements in card

--- src/components/AdmissionCard.jsx
+++ src/components/AdmissionCard.jsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card";
 const AdmissionCard = ({ item }) => {
   return (
     <Card className="w-3/4 h-[75vh] overflow-hidden ">
-      <CardContent className="w-full h-full">
+      <CardContent className="flex flex-col justify-center w-full h-full">
         <h1 className="text-xl py-4 text-center underline">{item.standard}</h1>
         <p className="text-sm tracking-tighter leading-5">
           <strong>Eligibility:</strong> {item.eligibility}
